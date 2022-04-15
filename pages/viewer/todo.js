e823f40ee193076315985475199f94d5ff991e43
CHansenDesign <hello@chansen.design>
2022-04-15 22:16:18 +0200

    reformatting todo.js

--- pages/viewer/todo.js
+++ pages/viewer/todo.js
@@ -1,11 +1,5 @@
-import React from 'react'
+import React from 'react';
 
-
-export default function ToDo(){
-        return (
-          <div>
-              This is a To Do List
-
-              Sara's project
-          </div> )
+export default function ToDo() {
+  return <div>This is a To Do List Sara's project</div>;
 }
