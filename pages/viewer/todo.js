42c681417f7e8d83957dd2aa7c4d5895b4b51760
CHansenDesign <hello@chansen.design>
2022-05-09 21:44:37 +0200

    squashing build bug

--- pages/viewer/todo.js
+++ pages/viewer/todo.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function ToDo() {
-  return <div>This is a To Do List Sara's project</div>;
+  return <div>{`This is a To Do List Sara's project`}</div>;
 }
